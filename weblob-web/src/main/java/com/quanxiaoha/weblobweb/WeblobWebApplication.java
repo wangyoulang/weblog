96a75b88656099d58e35893c36c4e2b39da2fec4
wangyoulang <1759923654@qq.com>
2024-04-01 23:55:47 +0800

    初始化仓库

---
+++ weblob-web/src/main/java/com/quanxiaoha/weblobweb/WeblobWebApplication.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.weblobweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblobWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblobWebApplication.class, args);
+    }
+
+}
